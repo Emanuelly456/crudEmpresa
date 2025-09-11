6b9c913ac6da4672d96e4a4602b24edbebb38933
Emanuelly <giliotiemanuelly3@gmail.com>
2025-09-11 20:39:32 -0300

    alteração service 11-09-25

--- src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/SERVICE/EmpresaService.java
@@ -1,17 +1,40 @@
 package com.example.crudSpring.projetoCRUD.SERVICE;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
 import com.example.crudSpring.projetoCRUD.REPOSITORY.EmpresaRepository;
 
 @Service
 public class EmpresaService {
     
-    private final EmpresaRepository empresaRepository;
+private final EmpresaRepository empresaRepository;
 
-   //método construtor da Classe EmpresaService
-   //criando uma ligção com a Classe EmpresaRepository
+ //método construtor da Classe EmpresaService
+ //criando uma ligção com a Classe EmpresaRepository
+
     public EmpresaService(EmpresaRepository ligacaoEmpresaRepository){
         empresaRepository = ligacaoEmpresaRepository;
     }
+    public List<Empresa> findAll() {
+        return empresaRepository.findAll();
+    }//select * from empresa(LISTAR)
+
+      //OBJECT-CRIAR ALGO NO BANCO
+   public Empresa cadastrarEmpresa(Empresa dadoEmpresa){
+        return empresaRepository.save(dadoEmpresa);
+   
+   }
+     // deletar alguma parte de banco
+   public void deletarEmpresa(Empresa dadoEmpresa){
+    empresaRepository.delete(dadoEmpresa);
+   }
+
 }
+//SELECTS OU INSERTS OU ALTER TABLE OU DELETE
+ //SELECT * FROM EMPRESA(SELECT REALIZA LISTAGEM DE DADOS)
+ //where nome_empresa = ?
+ //(1-SELECT   2-Quais os dados deseja listar 3-Onde deseja executar o comando)
+ //retorna todos os dados da empresa (* - tudo da tabela)
