63b49f1b8d0add18fc6232eedd034ca13e8711c5
Emanuelly <giliotiemanuelly3@gmail.com>
2025-09-29 20:41:24 -0300

    Alteração 29/09/25

--- src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/EmpresaController.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/EmpresaController.java
@@ -3,6 +3,8 @@ package com.example.crudSpring.projetoCRUD.CONTROLLER;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.crudSpring.projetoCRUD.ENTITY.Empresa;
@@ -23,6 +25,18 @@ public class EmpresaController {
    public String mostrarFormCadastro(Model oModel){
    oModel.addAttribute("empresa", new Empresa());
    return "cadastroEmpresa";
+   }
+
+   //chamada para listar todas as empresas
+
+   @PostMapping("/salvarEmpresa")
+   public String salvarEmpresa(@ModelAttribute Empresa objEmpresa){
+    //chamando o método cadastrar e passando
+    //o objeto("pacotinho") com os dados que precisam ser salvos
+    empresaService.cadastrarEmpresa(objEmpresa);
+    return "redirect:/empresaCTR/viewCadEmpresa";
+   
+
    }
 
 }
