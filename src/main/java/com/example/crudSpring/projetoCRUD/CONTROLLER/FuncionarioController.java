5274348222b5446794c78843b3e2c7e176818004
Emanuelly <giliotiemanuelly3@gmail.com>
2025-11-24 20:41:52 -0300

    Alteração 24/11/25

--- src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/FuncionarioController.java
+++ src/main/java/com/example/crudSpring/projetoCRUD/CONTROLLER/FuncionarioController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,34 +16,38 @@ import com.example.crudSpring.projetoCRUD.SERVICE.FuncionarioService;
 @Controller
 @RequestMapping("/funcionarioCTR")
 public class FuncionarioController {
-    
-    @Autowired
-    private FuncionarioService ligFuncionarioService;
 
-    @Autowired
-    private EmpresaService ligacaoEmpresaService;
+  @Autowired
+  private FuncionarioService ligFuncionarioService;
 
+  @Autowired
+  private EmpresaService ligacaoEmpresaService;
 
+  @GetMapping("/listarFunc")
+  public String listarTodosFuncionarios(Model oModel) {
+    oModel.addAttribute("funcionarios", ligFuncionarioService.listarTodosFuncionario());
+    // oModel.addAttribute("empresa", ligacaoEmpresaService.findAll());
 
-    @GetMapping("/listarFunc")
-    public String listarTodosFuncionarios(Model oModel){
-      oModel.addAttribute("funcionarios", ligFuncionarioService.listarTodosFuncionario());
-      //oModel.addAttribute("empresa", ligacaoEmpresaService.findAll());
+    return "listarFuncionario";
+  }
 
-      return "listarFuncionario";
-    }
-
-@GetMapping("/formFuncionario")
-public String mostrarFormCadastro(Model oModel){
-    oModel.addAttribute("funcionario",new Funcionario());
+  @GetMapping("/formFuncionario")
+  public String mostrarFormCadastro(Model oModel) {
+    oModel.addAttribute("funcionario", new Funcionario());
     oModel.addAttribute("empresas", ligacaoEmpresaService.findAll());
     return "cadastrarFuncionario";
-}
+  }
 
- @PostMapping("/salvarFuncionario")
-  public String CadastrarFuncionario(@ModelAttribute Funcionario objFuncionario){
+  @PostMapping("/salvarFuncionario")
+  public String CadastrarFuncionario(@ModelAttribute Funcionario objFuncionario) {
 
     ligFuncionarioService.cadastarFuncionario(objFuncionario);
+    return "redirect:/funcionarioCTR/listarFunc";
+  }
+
+  @GetMapping("/deletarFuncionario/{codigo}")
+  public String apagarFuncionario(@PathVariable("codigo") long codigo) {
+    ligFuncionarioService.deletarFuncionario(codigo);
     return "redirect:/funcionarioCTR/listarFunc";
   }
 
